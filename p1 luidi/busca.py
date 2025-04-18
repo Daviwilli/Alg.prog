f66e5216815377caf991aa30b991ebbc2c129c07
Davi <daviwilliam.20241@poli.ufrj.br>
2025-04-18 00:03:33 -0300

    refiz o codigo de busca

--- p1 luidi/busca.py
+++ p1 luidi/busca.py
@@ -1,34 +1,16 @@
-def marco_doido(L,X):
+def buscar(L, X):
     if len(L) == 0:
-       return -1
+        return -1
     elif len(L) == 1:
-        if L[0] == X:
-            return 0
-        else:
-            return -1
-    m = len(L)//2
+        return 0 if L[0] == X else -1
+    
+    m = len(L) // 2
     if L[m] == X:
-        return m 
-    if L[m] > X: return marco_doido(L[:m],X)
-    return m+ marco_doido(L[m+1],X)
-print(marco_doido([1,2,3,4,5,6,7],6))
+        return m
+    elif L[m] > X:
+        return buscar(L[:m], X)
+    else:
+        resultado = buscar(L[m+1:], X)
+        return m + 1 + resultado if resultado != -1 else -1
 
-def marco_doido(L,X):
-    if len(L) == 0: return -1
-    def marco_doido_revan(L,X,i):
-        if len(L) <= i: return -1
-        if L[i] > x: return marco_doido_revan(L,X,i)
-
-    if len(L) == 0:
-       return -1
-    elif len(L) == 1:
-        if L[0] == X:
-            return 0
-        else:
-            return -1
-    m = len(L)//2
-    if L[m] == X:
-        return m 
-    if L[m] > X: return marco_doido(L[:m],X)
-    return m+ marco_doido(L[m+1],X)
-print(marco_doido([1,2,3,4,5,6,7],6))
+print(buscar([1, 2, 3, 4, 5, 6, 7], 6))
---
+++ p1 luidi/carol.py
@@ -0,0 +1,3 @@
+A = [1,2,3,4,5]
+for i,j in enumerate(A):
+    print(i,j)
